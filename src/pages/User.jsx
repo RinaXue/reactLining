adcad7e1c490a38748b8f58996bf3203d9f61175
Teemowei <1061779661@qq.com>
2019-03-11 23:39:20 +0800

    添加了个人中心头部

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Userhead from '@/components/User/Userhead/Userhead'
 import Userlist from '@/components/User/Userlist/Userlist'
+import Head from '@/components/common/Head/Head'
 
 export default class Com extends Component{
     render() {
         return(
             <div className="content">
+              <Head src={this.props} title='个人中心'/>
               <Userhead/>
               <Userlist {...this.props}/>
             </div>
