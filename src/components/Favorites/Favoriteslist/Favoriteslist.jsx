f58d207f60897ac2812c363b5fc4029ae618621e
Teemowei <1061779661@qq.com>
2019-03-13 23:45:01 +0800

    增加了收藏

--- src/components/Favorites/Favoriteslist/Favoriteslist.jsx
+++ src/components/Favorites/Favoriteslist/Favoriteslist.jsx
@@ -1,11 +1,45 @@
 import React,{Component} from 'react'
 import './Favoriteslist.scss'
+import { Card, WhiteSpace } from 'antd-mobile';
 
 class Com extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      arr:[]
+    }
+  }
+  componentDidMount(){
+    this.setState({
+      arr:JSON.parse(localStorage.getItem('loveData'))
+    })
+  }
   render(){
+    let html = []
+      this.state.arr.map((item, index) => {
+        console.log(item,index)
+        html.push(
+        <div key={index}>
+          <WhiteSpace size="lg" />
+          <Card full>
+          <div id='tp'>
+            <Card.Header
+              title={item.goodsName}
+              thumb={item.mainPic}
+              // extra={<span>this is extra</span>}
+            />
+          </div>
+            {/* <Card.Body>
+              <div>This is content of `Card`</div>
+            </Card.Body> */}
+            <Card.Footer content={item.marketPrice} extra={<a>删除</a>} />
+          </Card>
+        </div>
+        )
+      })
     return(
       <div className="content">
-        我的收藏
+        {html}
       </div>
     )
   }
